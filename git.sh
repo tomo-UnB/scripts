5fda40afef93027d8c0ebdf4d868eee23677f97d
Marcelo Peres Rocha <marcelo@suporte@application.unb.br>
2015-09-10 10:44:12 -0300

    Atualização Qui Set 10 10:43:56 BRT 2015: "teste tomo-UnB"

--- git.sh
+++ git.sh
@@ -5,7 +5,7 @@ date=`date`
 read -p "Digite o comentario sobre as modificações feitas: " comment
 
 
-#git init
+git init
 git add .
 git commit -m "Atualização $date: $comment"
 git push origin master
